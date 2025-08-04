ee2f301a532207d3c660b8c733728f011b76db79
rafasantosit4iot <rafael.santos@it4iot.com>
2025-08-04 17:33:28 -0300

    👌Criação de relacionamento entre entidades funcionário e cargo

--- people-sync-backend/src/main/java/com/example/people_sync_backend/features/employee/model/Employee.java
+++ people-sync-backend/src/main/java/com/example/people_sync_backend/features/employee/model/Employee.java
@@ -11,9 +11,11 @@ import com.example.people_sync_backend.features.employee.model.enums.EmployeeTyp
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -53,6 +55,7 @@ public class Employee {
     private EmployeeGender gender;
     private boolean active;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", referencedColumnName = "id", nullable = false)
     private Role role;
 }
--- people-sync-backend/src/main/java/com/example/people_sync_backend/features/role/model/Role.java
+++ people-sync-backend/src/main/java/com/example/people_sync_backend/features/role/model/Role.java
@@ -1,9 +1,15 @@
 package com.example.people_sync_backend.features.role.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.example.people_sync_backend.features.employee.model.Employee;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,4 +28,7 @@ public class Role {
     private Integer id;
 
     private String name;
+
+    @OneToMany(mappedBy = "employee")
+    private Set<Employee> employees = new HashSet<>();
 }
