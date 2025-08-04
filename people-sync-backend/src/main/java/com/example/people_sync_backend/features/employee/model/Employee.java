84f69c96e557b3416b0e8bca3a9a57955eb87b83
rafasantosit4iot <rafael.santos@it4iot.com>
2025-08-04 17:21:05 -0300

    ✨Criação da entidade de funcionário

--- people-sync-backend/src/main/java/com/example/people_sync_backend/features/employee/model/Employee.java
+++ people-sync-backend/src/main/java/com/example/people_sync_backend/features/employee/model/Employee.java
@@ -1,14 +1,20 @@
 package com.example.people_sync_backend.features.employee.model;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
+import javax.management.relation.Role;
+
 import com.example.people_sync_backend.features.employee.model.enums.ContractType;
-import com.example.people_sync_backend.features.employee.model.enums.EmployeeRole;
+import com.example.people_sync_backend.features.employee.model.enums.EmployeeGender;
 import com.example.people_sync_backend.features.employee.model.enums.EmployeeType;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,22 +26,33 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name="employees")
+@Table(name = "employees")
 public class Employee {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    @Column(length=40)
+    @Column(length = 40)
     private String name;
 
-    @Column(unique=true)
+    @Column(unique = true)
     private String email;
 
-    private EmployeeRole role;
     private ContractType contract;
 
-    @Column(unique=true)
+    @Column(unique = true)
     private String register;
 
     private EmployeeType type;
+
+    @Column(unique = true)
+    private String password;
+
+    private boolean pcd;
+    private LocalDate birthday;
+    private EmployeeGender gender;
+    private boolean active;
+
+    @ManyToOne
+    private Role role;
 }
