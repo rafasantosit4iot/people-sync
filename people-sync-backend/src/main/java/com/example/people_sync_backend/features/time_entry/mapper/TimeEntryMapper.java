586c4eb192f17b9bf66b9e018ee4c7b1e23bd869
rafasantosit4iot <rafael.santos@it4iot.com>
2025-08-07 09:09:23 -0300

    ✨Criação do mapeamento da entidade e DTOs das entradas de ponto

--- people-sync-backend/src/main/java/com/example/people_sync_backend/features/time_entry/mapper/TimeEntryMapper.java
+++ people-sync-backend/src/main/java/com/example/people_sync_backend/features/time_entry/mapper/TimeEntryMapper.java
@@ -1,13 +1,66 @@
 package com.example.people_sync_backend.features.time_entry.mapper;
 
+import java.security.Timestamp;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
+import com.example.people_sync_backend.features.employee.mapper.EmployeeSummaryConverter;
+import com.example.people_sync_backend.features.employee.model.dto.response.EmployeeSummaryDTO;
+import com.example.people_sync_backend.features.project.mapper.ProjectSummaryConverter;
+import com.example.people_sync_backend.features.project.model.dto.response.ProjectSummaryDTO;
 import com.example.people_sync_backend.features.time_entry.model.TimeEntry;
 import com.example.people_sync_backend.features.time_entry.model.dto.request.TimeEntryCreateDTO;
 import com.example.people_sync_backend.features.time_entry.model.dto.response.TimeEntryResponseDTO;
 import com.example.people_sync_backend.shared.interfaces.EntityMapper;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
-public class TimeEntryMapper implements EntityMapper<TimeEntry, TimeEntryCreateDTO, TimeEntryResponseDTO>{
-    
+@RequiredArgsConstructor
+public class TimeEntryMapper implements EntityMapper<TimeEntry, TimeEntryCreateDTO, TimeEntryResponseDTO> {
+
+    private final EmployeeSummaryConverter employeeSummaryConverter;
+    private final ProjectSummaryConverter projectSummaryConverter;
+
+    @Override
+    public TimeEntry toEntity(TimeEntryCreateDTO timeEntryCreateDTO) {
+        TimeEntry timeEntry = new TimeEntry();
+
+        timeEntry.setOpenTimestamp(timeEntryCreateDTO.openTimestamp());
+        timeEntry.setCloseTimestamp(timeEntryCreateDTO.closeTimestamp());
+        timeEntry.setDescription(timeEntryCreateDTO.description());
+
+        timeEntry.setEmployee(employee);
+        timeEntry.setProject(project);
+
+        return timeEntry;
+    }
+
+    @Override
+    public TimeEntryResponseDTO toDTOResponse(TimeEntry timeEntry) {
+        Timestamp openTimestamp = timeEntry.getOpenTimestamp();
+        Timestamp closeTimestamp = timeEntry.getCloseTimestamp();
+        String description = timeEntry.getDescription();
+
+        EmployeeSummaryDTO employee = employeeSummaryConverter.toSummaryDTO(timeEntry.getEmployee());
+        ProjectSummaryDTO project = projectSummaryConverter.toSummaryDTO(timeEntry.getProject());
+
+        return new TimeEntryResponseDTO(openTimestamp, closeTimestamp, description, employee, project);
+    }
+
+    @Override
+    public Page<TimeEntryResponseDTO> toDTOPageResponse(Page<TimeEntry> timeEntries) {
+        return timeEntries.map(this::toDTOResponse);
+    }
+
+    @Override
+    public List<TimeEntryResponseDTO> toDTOListResponse(Collection<TimeEntry> timeEntries) {
+        return timeEntries.stream()
+                .map(this::toDTOResponse)
+                .collect(Collectors.toList());
+    }
 }
--- people-sync-backend/src/main/java/com/example/people_sync_backend/features/time_entry/mapper/TimeEntrySummaryConverter.java
+++ people-sync-backend/src/main/java/com/example/people_sync_backend/features/time_entry/mapper/TimeEntrySummaryConverter.java
@@ -1,5 +1,9 @@
 package com.example.people_sync_backend.features.time_entry.mapper;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 
 import com.example.people_sync_backend.features.time_entry.model.TimeEntry;
@@ -7,6 +11,24 @@ import com.example.people_sync_backend.features.time_entry.model.dto.response.Ti
 import com.example.people_sync_backend.shared.interfaces.EntitySummaryConverter;
 
 @Component
-public class TimeEntrySummaryConverter implements EntitySummaryConverter<TimeEntry, TimeEntrySummaryDTO>{
-    
+public class TimeEntrySummaryConverter implements EntitySummaryConverter<TimeEntry, TimeEntrySummaryDTO> {
+
+    @Override
+    public TimeEntrySummaryDTO toSummaryDTO(TimeEntry timeEntry) {
+        return new TimeEntrySummaryDTO(
+                timeEntry.getOpenTimestamp(),
+                timeEntry.getCloseTimestamp(),
+                timeEntry.getDescription(),
+                timeEntry.getEmployee().getId(),
+                timeEntry.getEmployee().getName(),
+                timeEntry.getProject().getId(),
+                timeEntry.getProject().getName());
+    }
+
+    @Override
+    public List<TimeEntrySummaryDTO> toSummaryListDTO(Collection<TimeEntry> timeEntries) {
+        return timeEntries.stream()
+                .map(this::toSummaryDTO)
+                .collect(Collectors.toList());
+    }
 }
