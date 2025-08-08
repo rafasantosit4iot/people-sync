6b32e6b961b07e9f129acd1120fa95ecfe2fc7da
rafasantosit4iot <rafael.santos@it4iot.com>
2025-08-08 17:29:56 -0300

    👌Ajuste na nomenclatura de entidades genéricas no Mapper

--- people-sync-backend/src/main/java/com/example/people_sync_backend/shared/classes/EntityMapper.java
+++ people-sync-backend/src/main/java/com/example/people_sync_backend/shared/classes/EntityMapper.java
@@ -6,17 +6,17 @@ import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 
-public abstract class EntityMapper<Entity, CreateEntity, ResponseEntity> {
+public abstract class EntityMapper<Base, Create, Response> {
 
-    public abstract Entity toEntity(CreateEntity entityDTO);
+    public abstract Base toEntity(Create entityDTO);
 
-    public abstract ResponseEntity toDTOResponse(Entity entity);
+    public abstract Response toDTOResponse(Base entity);
 
-    public Page<ResponseEntity> toDTOPageResponse(Page<Entity> entityPage) {
+    public Page<Response> toDTOPageResponse(Page<Base> entityPage) {
         return entityPage.map(this::toDTOResponse);
     }
 
-    public List<ResponseEntity> toDTOListResponse(Collection<Entity> entityCollection) {
+    public List<Response> toDTOListResponse(Collection<Base> entityCollection) {
         return entityCollection.stream()
                 .map(this::toDTOResponse)
                 .collect(Collectors.toList());
