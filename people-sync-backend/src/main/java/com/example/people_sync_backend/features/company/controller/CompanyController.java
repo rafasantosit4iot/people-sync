79640b62eeffaa9ede336795763f797b35d42aa5
rafasantosit4iot <rafael.santos@it4iot.com>
2025-08-08 17:26:10 -0300

    👌Implementação da entidade genérica de Controller para Company

--- people-sync-backend/src/main/java/com/example/people_sync_backend/features/company/controller/CompanyController.java
+++ people-sync-backend/src/main/java/com/example/people_sync_backend/features/company/controller/CompanyController.java
@@ -1,17 +1,20 @@
 package com.example.people_sync_backend.features.company.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.people_sync_backend.features.company.model.dto.request.CompanyCreateDTO;
+import com.example.people_sync_backend.features.company.model.dto.response.CompanyResponseDTO;
+import com.example.people_sync_backend.features.company.service.CompanyService;
+import com.example.people_sync_backend.shared.classes.EntityController;
 
 @RestController
 @RequestMapping("/companies")
 @CrossOrigin(origins="http://localhost:4202")
-public class CompanyController {
-    
-    @GetMapping
-    public String getCompanies() {
-        return "Empresas";
+public class CompanyController extends EntityController<CompanyCreateDTO, CompanyResponseDTO, CompanyService> {
+
+    public CompanyController(CompanyService service) {
+        super(service);
     }
 }
